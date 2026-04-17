8dc428a188ba46f918827671432604b64948ae77
孫健太 <45735033+sxd19911008@users.noreply.github.com>
2026-04-17 16:28:13 +0800

    optimize: 删除testParamsOrder案例生成代码。

--- src/test/java/com/eredar/aviatororacle/DecodeFunctionTest.java
+++ src/test/java/com/eredar/aviatororacle/DecodeFunctionTest.java
@@ -8,9 +8,7 @@ import com.eredar.aviatororacle.testUtils.HashMapBuilder;
 import com.eredar.aviatororacle.testUtils.JsonUtils;
 import com.fasterxml.jackson.core.type.TypeReference;
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
@@ -445,70 +443,6 @@ public class DecodeFunctionTest {
         } else {
             Object actual = AviatorInstance.execute(expression, vars);
             Assertions.assertEquals(expected, actual);
-        }
-    }
-
-    @Disabled
-    @Test
-    public void buildParamsOrderTestCase() {
-        StringBuilder sb = new StringBuilder();
-        for (int times = 1; times <= 12; times++) {
-            for (int value = 1; value <= times + 1; value++) {
-                // 有默认值
-                this.buildCase(sb, times, value, false);
-                // 无默认值
-                this.buildCase(sb, times, value, true);
-            }
-        }
-        System.err.println(sb);
-    }
-
-    private void buildCase(StringBuilder sb, int times, int value, boolean hasDefault) {
-        sb.append("{\"expression\":");
-        sb.append(this.getExpression(times, hasDefault));
-        sb.append(",\"env\":{\"i\":").append(value);
-        sb.append(this.getPutSb(times, hasDefault));
-        sb.append("},\"expected\":");
-        sb.append(this.getExpected(value, times, hasDefault));
-        sb.append("}\n");
-    }
-
-    private StringBuilder getExpression(int times, boolean hasDefault) {
-        StringBuilder sb = new StringBuilder();
-        sb.append("\"decode(i");
-
-        for (int i = 1; i <= times; i++) {
-            sb.append(String.format(", c%s, r%s", i, i));
-        }
-
-        if (hasDefault) {
-            sb.append(", d");
-        }
-
-        sb.append(")\"");
-        return sb;
-    }
-
-    private StringBuilder getPutSb(int times, boolean hasDefault) {
-        StringBuilder sb = new StringBuilder();
-        for (int i = 1; i <= times; i++) {
-            sb.append(String.format(",\"c%s\":%s,\"r%s\":\"R%s\"", i, i, i, i));
-        }
-        if (hasDefault) {
-            sb.append(",\"d\":\"default\"");
-        }
-        return sb;
-    }
-
-    private String getExpected(int value, int times, boolean hasDefault) {
-        if (value <= times) {
-            return String.format("\"R%s\"", value);
-        } else {
-            if (hasDefault) {
-                return "\"default\"";
-            } else {
-                return "null";
-            }
         }
     }
 }
